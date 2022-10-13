4f5f23aa96b7236c06aca08c0112e423c757848f
Michael <michaelgandolfi7@gmail.com>
2022-10-13 11:23:08 -0500

    added conditionals to be able to count

--- js/main.js
+++ js/main.js
@@ -23,10 +23,34 @@ function drawTwo() {
             console.log(data)
             document.querySelector('#player1').src = data.cards[0].image
             document.querySelector('#player2').src = data.cards[1].image
+            let player1Value = convertToNum(data.cards[0].value)
+            let player2Value = convertToNum(data.cards[1].value)
+
+            if (player1Value > player2Value) {
+                document.querySelector('h3').innerText = "Player 1 Wins"
+            } else if (player1Value < player2Value) {
+                document.querySelector('h3').innerText = "Player 2 Wins"
+            } else {
+                document.querySelector('h3').innerText = "TIME FOR WAR!"
+            }
 
     })
 
     .catch(err => {
         console.log(`error ${err}`)
     });
+}
+
+function convertToNum(val) {
+    if (val === "ACE") {
+        return 14
+    } else if (val === "KING") {
+        return 13
+    } else if (val === "QUEEN") {
+        return 12
+    } else if (val === "JACK") {
+        return 11
+    } else {
+        return val
+    }
 }
