e2d860f0ceaee2d19ae8bd7b456543a4b26e4e46
Michael <michaelgandolfi7@gmail.com>
2022-10-15 11:13:54 -0500

    removed incorrect description comment

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-//The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
 document.querySelector('button').addEventListener('click', drawTwo)
 let deckId = ""
 
